381bd1df9c1b943f059b6053e0e24a4491dcac7e
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-01-31 23:19:16 +0200

    feat: added tests for review

---
+++ src/review/review.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ReviewService } from './review.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Review } from './review.schema';
+import { Types } from 'mongoose';
+
+describe('ReviewService', () => {
+  let service: ReviewService;
+
+  const exec = { exec: jest.fn() };
+
+  const reviewRepositoryFactory = () => ({
+    find: () => exec,
+  });
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ReviewService,
+        {
+          useFactory: reviewRepositoryFactory,
+          provide: getModelToken(Review.name),
+        },
+      ],
+    }).compile();
+
+    service = module.get<ReviewService>(ReviewService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('findByProductId', async () => {
+    const id = new Types.ObjectId().toHexString();
+    reviewRepositoryFactory()
+      .find()
+      .exec.mockReturnValueOnce([{ productId: id }]);
+
+    const res = await service.findByProductId(id);
+    expect(res[0].productId).toBe(id);
+  });
+});
