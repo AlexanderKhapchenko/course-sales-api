f3a83ff14f3ff0883ff90137844fc5e6f29a922e
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-02-01 23:16:51 +0200

    fix: repaired tests for protected route

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -5,6 +5,13 @@ import { AppModule } from '../src/app.module';
 import { CreateReviewDto } from '../src/review/dto/create-review.dto';
 import { Types, disconnect } from 'mongoose';
 import { REVIEW_NOT_FOUND } from '../src/review/review.constants';
+import { AuthDto } from '../src/auth/dto/auth.dto';
+
+// TODO: add methods for registration and deletion this user. For now, you need to add this user before testing the app
+const login: AuthDto = {
+  login: 'test-user@gmail.com',
+  password: 'test-password',
+};
 
 const productId = new Types.ObjectId().toHexString();
 
@@ -19,6 +26,7 @@ const testDto: CreateReviewDto = {
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let createdId: string;
+  let token: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -27,6 +35,12 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const { body } = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(login);
+
+    token = body.accessToken;
   });
 
   it('/review/create (POST) - success', () => {
@@ -68,12 +82,14 @@ describe('AppController (e2e)', () => {
   it('/review/:id (DELETE) - success', () => {
     return request(app.getHttpServer())
       .delete('/review/' + createdId)
+      .set('Authorization', 'Bearer ' + token)
       .expect(200);
   });
 
   it('/review/:id (DELETE) - fail', () => {
     return request(app.getHttpServer())
       .delete('/review/' + new Types.ObjectId().toHexString())
+      .set('Authorization', 'Bearer ' + token)
       .expect(404, {
         statusCode: 404,
         message: REVIEW_NOT_FOUND,
