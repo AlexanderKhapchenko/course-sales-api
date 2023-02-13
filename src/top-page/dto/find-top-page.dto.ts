50e33ca326b6126ffc15f729f0df4afc57e49554
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-02-14 00:26:05 +0200

    feat: added validation to find top page dto

--- src/top-page/dto/find-top-page.dto.ts
+++ src/top-page/dto/find-top-page.dto.ts
@@ -1,5 +1,7 @@
 import { TopLevelCategory } from '../top-page.schema';
+import { IsEnum } from 'class-validator';
 
 export class FindTopPageDto {
+  @IsEnum(TopLevelCategory)
   firstCategory: TopLevelCategory;
 }
