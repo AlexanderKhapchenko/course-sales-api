279df8467536000a47a28b5c048f6ca15fdd3e29
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-02-14 00:26:22 +0200

    feat: added create top page dto

---
+++ src/top-page/dto/create-top-page.dto.ts
@@ -0,0 +1,69 @@
+import { TopLevelCategory } from '../top-page.schema';
+import {
+  IsArray,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class HhDataDto {
+  @IsNumber()
+  count: number;
+
+  @IsNumber()
+  juniorSalary: number;
+
+  @IsNumber()
+  middleSalary: number;
+
+  @IsNumber()
+  seniorSalary: number;
+}
+
+class TopPageAdvantagesDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+}
+
+export class CreateTopPageDto {
+  @IsEnum(TopLevelCategory)
+  firstCategory: TopLevelCategory;
+
+  @IsString()
+  secondCategory: string;
+
+  @IsString()
+  alias: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => HhDataDto)
+  hh?: HhDataDto;
+
+  @IsArray()
+  @ValidateNested()
+  @Type(() => TopPageAdvantagesDto)
+  advantages: TopPageAdvantagesDto[];
+
+  @IsString()
+  seoText: string;
+
+  @IsString()
+  tagsTitle: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
