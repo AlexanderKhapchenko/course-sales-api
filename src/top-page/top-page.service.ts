7370ce4d07514d1c1394fc0087d4ce2563bfcf50
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-02-15 21:13:22 +0200

    feat: changed return values with grouping information

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -25,7 +25,21 @@ export class TopPageService {
 
   async findByCategory(firstCategory: TopLevelCategory) {
     return this.topPageModel
-      .find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 })
+      .aggregate()
+      .match({
+        firstCategory,
+      })
+      .group({
+        _id: {
+          secondCategory: '$secondCategory',
+        },
+        pages: {
+          $push: {
+            alias: '$alias',
+            title: '$title',
+          },
+        },
+      })
       .exec();
   }
 
